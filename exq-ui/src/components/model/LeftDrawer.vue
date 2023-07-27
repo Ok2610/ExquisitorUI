91f9c9f078e24338986d34868aa5bc40a5eaf8d8
Omar Shahbaz Khan <omark@ru.is>
2023-07-27 10:43:39 +0000

    Add left and right navigation drawers to model page.

---
+++ exq-ui/src/components/model/LeftDrawer.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+
+interface Props {
+    modelId: number
+}
+defineProps<Props>()
+
+const rail = ref(true)
+
+</script>
+
+<template>
+    <v-navigation-drawer
+     theme="dark"
+     rail
+    >
+        <v-list>
+            <v-list-item
+             prepend-icon="mdi-format-list-bulleted"
+             @click="console.log('clicked filter')"
+            >
+            </v-list-item>
+            <v-list-item
+             prepend-icon="mdi-magnify"
+             @click="console.log('clicked search')"
+            >
+
+            </v-list-item>
+            <v-list-item
+             prepend-icon="mdi-call-merge"
+             @click="console.log('clicked merge')"
+            >
+
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
---
+++ exq-ui/src/components/model/RightDrawer.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+
+interface Props {
+    modelId: number
+}
+defineProps<Props>()
+
+const rail = ref(true)
+
+</script>
+
+<template>
+    <v-navigation-drawer
+     theme="dark"
+     location="right"
+     rail
+    >
+        <v-list>
+            <v-list-item
+             prepend-icon="mdi-thumb-up-outline"
+             @click="console.log('clicked positives')"
+            >
+            </v-list-item>
+            <v-list-item
+             prepend-icon="mdi-thumb-down-outline"
+             @click="console.log('clicked negatives')"
+            >
+
+            </v-list-item>
+            <v-list-item
+             prepend-icon="mdi-history"
+             @click="console.log('clicked history')"
+            >
+
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
