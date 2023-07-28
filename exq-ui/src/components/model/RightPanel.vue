afd1e9e7b2532ba97953c588597e43530a840e08
Omar Khan <ok2610@gmail.com>
2023-07-28 22:31:24 +0200

    Added drawers for positive, negative and history

--- exq-ui/src/components/model/RightPanel.vue
+++ exq-ui/src/components/model/RightPanel.vue
@@ -1,17 +1,65 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { useItemStore } from '@/stores/items';
+import { ILSets, ItemButton, type ItemButtons } from '@/types/mediaitem';
+import { computed, reactive, ref } from 'vue';
+import Item from '@/components/model/Item.vue';
+import type MediaItem from '@/types/mediaitem';
 
 
 interface Props {
     modelId: number
 }
-defineProps<Props>()
+const props = defineProps<Props>()
 
-const rail = ref(true)
+const itemStore = useItemStore()
+const getSetItems = itemStore.getSetItems
 
+const posButtonSet = new Set<ItemButton>()
+posButtonSet.add(ItemButton.Neg)
+posButtonSet.add(ItemButton.Ignore)
+posButtonSet.add(ItemButton.Sub)
+const posButtons : ItemButtons = { buttons: posButtonSet }
+
+const negButtonSet = new Set<ItemButton>()
+negButtonSet.add(ItemButton.Pos)
+negButtonSet.add(ItemButton.Ignore)
+negButtonSet.add(ItemButton.Sub)
+const negButtons : ItemButtons = { buttons: negButtonSet }
+
+const histButtonSet = new Set<ItemButton>()
+histButtonSet.add(ItemButton.Neg)
+histButtonSet.add(ItemButton.Sub)
+const histButtons : ItemButtons = { buttons: histButtonSet }
+
+const posToggle = ref(false)
+const negToggle = ref(false)
+const histToggle = ref(false)
+
+const sets = reactive({
+    positives : [] as MediaItem[],
+    negatives : [] as MediaItem[],
+    history : [] as MediaItem[],
+})
+
+function getPositives() {
+    sets.positives = getSetItems(props.modelId, ILSets.Positives)
+}
+
+function getNegatives() {
+    sets.negatives = getSetItems(props.modelId, ILSets.Negatives)
+}
+
+function getHistory() {
+    sets.history = getSetItems(props.modelId, ILSets.History)
+}
+
+const posUpdate = computed(() => getPositives())
+const negUpdate = computed(() => getNegatives())
+const histUpdate = computed(() => getHistory())
 </script>
 
 <template>
+
     <v-navigation-drawer
      theme="dark"
      location="right"
@@ -20,7 +68,7 @@ const rail = ref(true)
         <v-list>
             <v-list-item
              prepend-icon="mdi-thumb-up-outline"
-             @click="console.log('clicked positives')"
+             @click="console.log('clicked positives'); posToggle = !posToggle; getPositives()"
             >
             </v-list-item>
 
@@ -28,7 +76,7 @@ const rail = ref(true)
 
             <v-list-item
              prepend-icon="mdi-thumb-down-outline"
-             @click="console.log('clicked negatives')"
+             @click="console.log('clicked negatives'); negToggle = !negToggle; getNegatives()"
             >
             </v-list-item>
 
@@ -36,10 +84,58 @@ const rail = ref(true)
 
             <v-list-item
              prepend-icon="mdi-history"
-             @click="console.log('clicked history')"
+             @click="console.log('clicked history'); histToggle = !histToggle; getHistory()"
             >
-
             </v-list-item>
         </v-list>
     </v-navigation-drawer>
+
+    <v-navigation-drawer 
+     v-if="posToggle"
+     location="right"
+     color="success"
+     >
+        <v-list :v-model="posUpdate">
+            <v-list-item v-for="it in sets.positives">
+                <item 
+                 :buttons="posButtons" 
+                 :item="it"
+                 :modelId="modelId"
+                />
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    
+    <v-navigation-drawer 
+     v-if="negToggle"
+     location="right"
+     color="error"
+     >
+        <v-list :v-model="negUpdate">
+            <v-list-item v-for="it in sets.negatives">
+                <item 
+                 :buttons="negButtons" 
+                 :item="it"
+                 :modelId="modelId"
+                />
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer 
+     v-if="histToggle"
+     location="right"
+     color="grey"
+     >
+        <v-list :v-model="histUpdate">
+            <v-list-item v-for="it in sets.history">
+                <item 
+                 :buttons="histButtons" 
+                 :item="it"
+                 :modelId="modelId"
+                />
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
 </template>
--- exq-ui/src/stores/items.ts
+++ exq-ui/src/stores/items.ts
@@ -6,12 +6,21 @@ import { reactive } from "vue";
 
 export const useItemStore = defineStore('item', () => {
     const items : Map<number, MediaItem> = reactive(new Map<number,MediaItem>())
+    const modelItems : Map<number, Set<number>> = reactive(new Map<number,Set<number>>())
     
     async function getMediaItem(exqId: number, modelId: number) : Promise<MediaItem> {
         if (items.has(exqId)) {
             return items.get(exqId)! // '!' Non-null
         } else {
-            return await getItem(exqId, modelId)
+            const item = await getItem(exqId, modelId)
+            items.set(exqId, item)
+            if (modelItems.has(modelId)) {
+                modelItems.get(modelId)?.add(exqId)
+            } else {
+                modelItems.set(modelId, new Set<number>())
+                modelItems.get(modelId)?.add(exqId)
+            }
+            return item
         }
     }
     
@@ -21,7 +30,16 @@ export const useItemStore = defineStore('item', () => {
             if (items.has(exqIds[i])) {
                 mediaItems.push(items.get(exqIds[i])!)
             } else {
-                mediaItems.push(await getItem(exqIds[i], modelId))
+                const item = await getItem(i, modelId)
+                console.log('Inserting ', item)
+                items.set(i, item)
+                if (modelItems.has(modelId)) {
+                    modelItems.get(modelId)?.add(i)
+                } else {
+                    modelItems.set(modelId, new Set<number>())
+                    modelItems.get(modelId)?.add(i)
+                }
+                mediaItems.push(item)
             }    
         }
         return mediaItems
@@ -29,7 +47,22 @@ export const useItemStore = defineStore('item', () => {
 
     function addItemToSet(exqId: number, modelId: number, ilset: ILSets) : boolean {
         console.log('Adding Item:', exqId, 'to set', ilset)
-        return items.get(exqId)?.currentSets.get(modelId)?.add(ilset) == null
+        switch (ilset) {
+            case ILSets.Positives:
+                removeItemFromSet(exqId, modelId, ILSets.Negatives)
+                removeItemFromSet(exqId, modelId, ILSets.History)
+                return items.get(exqId)?.currentSets.get(modelId)?.add(ilset) == null
+            case ILSets.Negatives:
+                removeItemFromSet(exqId, modelId, ILSets.Positives)
+                removeItemFromSet(exqId, modelId, ILSets.History)
+                return items.get(exqId)?.currentSets.get(modelId)?.add(ilset) == null
+            case ILSets.History:
+                removeItemFromSet(exqId, modelId, ILSets.Positives)
+                removeItemFromSet(exqId, modelId, ILSets.Negatives)
+                return items.get(exqId)?.currentSets.get(modelId)?.add(ilset) == null
+            default:
+                return items.get(exqId)?.currentSets.get(modelId)?.add(ilset) == null
+        }
     }
     
     function addItemsToSet(exqIds: number[], modelId: number, ilset: ILSets) : boolean {
@@ -68,18 +101,30 @@ export const useItemStore = defineStore('item', () => {
     }
 
     function isItemInNeg(exqId: number, modelId: number) : boolean {
-        if (items.get(exqId)?.currentSets.get(modelId)?.has(ILSets.Positives)) return true
+        if (items.get(exqId)?.currentSets.get(modelId)?.has(ILSets.Negatives)) return true
         return false
     }
 
     function isItemInSubmitted(exqId: number, modelId: number) : boolean {
-        if (items.get(exqId)?.currentSets.get(modelId)?.has(ILSets.Positives)) return true
+        if (items.get(exqId)?.currentSets.get(modelId)?.has(ILSets.Submitted)) return true
         return false
     }
 
     function isItemInHistory(exqId: number, modelId: number) : boolean {
-        if (items.get(exqId)?.currentSets.get(modelId)?.has(ILSets.Positives)) return true
+        if (items.get(exqId)?.currentSets.get(modelId)?.has(ILSets.History)) return true
         return false
+    }
+
+    function getSetItems(modelId: number, set: ILSets) : MediaItem[] {
+        let setItems : MediaItem[] = []
+        // Get all items for the current model
+        let mItems = modelItems.get(modelId)
+        mItems?.forEach((value, value2, s) => {
+            // Check if item is in the positive set
+            if (items.get(value)?.currentSets.get(modelId)?.has(set))
+                setItems.push(items.get(value)!)
+        })
+        return setItems
     }
 
     return {
@@ -94,6 +139,7 @@ export const useItemStore = defineStore('item', () => {
         isItemInPos,
         isItemInNeg,
         isItemInHistory,
-        isItemInSubmitted
+        isItemInSubmitted,
+        getSetItems,
     }
 })
